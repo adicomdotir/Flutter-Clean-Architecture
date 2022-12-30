692b10a4d24ad2b1bc55d712793eccc30d3793a2
adicomdotir <adicom.ir@gmail.com>
2022-12-30 21:49:36 +0330

    Add data state section

---
+++ test/core/states/data_state_test.dart
@@ -0,0 +1,68 @@
+import 'package:flutter_clean_arch/core/states/data_state.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../mocks/mock_users.dart';
+
+void main() {
+  group('DataState', () {
+    void buildExpectations<T>(
+      DataState<T> state, {
+      final bool isInProgress = false,
+      final bool isFailure = false,
+      final bool isSuccess = false,
+      final bool isEmpty = false,
+      final T? data,
+    }) {
+      expect(state.isInProgress, isInProgress);
+      expect(state.isFailure, isFailure);
+      expect(state.isSuccess, isSuccess);
+      expect(state.isEmpty, isEmpty);
+      expect(state.data, equals(data));
+    }
+
+    test('should return default values', () {
+      buildExpectations(DataState.initial());
+    });
+
+    test(
+      'should return [isInProgress = true]',
+      () {
+        buildExpectations(
+          DataState.inProgress(),
+          isInProgress: true,
+        );
+      },
+    );
+
+    test(
+      'should return [isFailure = true]',
+      () {
+        buildExpectations(
+          DataState.failure(),
+          isFailure: true,
+        );
+      },
+    );
+
+    test(
+      'should return [isEmpty = true]',
+      () {
+        buildExpectations(
+          DataState.empty(),
+          isEmpty: true,
+        );
+      },
+    );
+
+    test(
+      'should return [isSuccess = true] and data',
+      () {
+        buildExpectations(
+          DataState.success(users),
+          isSuccess: true,
+          data: users,
+        );
+      },
+    );
+  });
+}
