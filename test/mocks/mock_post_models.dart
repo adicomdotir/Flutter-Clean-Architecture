a31d5df864b1cfffbaa1a8d65e4f9391eba491bf
adicomdotir <adicom.ir@gmail.com>
2022-12-31 01:27:31 +0330

    Add post model

---
+++ test/mocks/mock_post_models.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_clean_arch/data/models/post_model.dart';
+
+final postModels = <PostModel>[
+  PostModel(
+    userId: 1,
+    id: 1,
+    title: 'Post Title 1',
+    body: 'Post Body 1',
+  ),
+  PostModel(
+    userId: 1,
+    id: 2,
+    title: 'Post Title 2',
+    body: 'Post Body 2',
+  ),
+];
